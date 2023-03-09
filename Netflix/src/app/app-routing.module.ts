a675f297ffa26df0475c4010d27470c7564f7246
Mariana <mariana.daruix@gmail.com>
2023-03-08 22:10:58 -0300

    atualiza

--- Netflix/src/app/app-routing.module.ts
+++ Netflix/src/app/app-routing.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
+import { FullComponent } from './layouts/full/full.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SerieComponent } from './pages/serie/serie.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'serie', component: SerieComponent, canActivate: [AuthGuard]},
-  { path: '**', redirectTo: 'home'}
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: 'home' },
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'serie', component: SerieComponent, canActivate: [AuthGuard] },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
